9fae84c2687ea389eb80aca646a3c77890a75c7b
carlosj.manosalvar@gmail.com <renault4>
2019-04-11 23:53:59 -0500

    Se crea un footer temporal

---
+++ footer.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Rich Store Lite
+ */
+?>
+
+<footer class="footer-distributed">
+
+			<div class="footer-right">
+
+				<a href="#"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-instagram"></i></a>
+
+			</div>
+
+			<div class="footer-left">
+
+				<p class="footer-links">
+					<a href="/blog/">Blog</a>
+					·
+					<a href="/welcome/">About</a>
+					·
+					<a href="#">Contact</a>
+				</p>
+
+				<p>doitsy &copy; 2019</p>
+			</div>
+
+		</footer>
+</div><!--#end sitelayout_options-->
+
+<?php wp_footer(); ?>
+</body>
+</html>
